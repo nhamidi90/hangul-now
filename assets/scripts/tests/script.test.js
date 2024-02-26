b6ee8866c4262b01a237a25f08b6cc5ccca9ee9c
nhamidi90 <nabilahhamidi@hotmail.com>
2024-02-26 18:12:02 +0000

    Add some tests

--- assets/scripts/tests/script.test.js
+++ assets/scripts/tests/script.test.js
@@ -2,7 +2,8 @@
  * @jest-environment jsdom
  */
 
-const flashcard = require("../script");
+// const flashcard = require("../script");
+const  { conClick } = require("../script");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -16,5 +17,25 @@ describe("DOM tests", () => {
     test("expect headers to exist", () => {
         expect(document.getElementsByTagName("h2").length).toBe(2);
     });
+    test("expect cards to exist", () => {
+        expect(document.getElementsByClassName("card").length).toBe(24);
+    });
 })
 
+describe("button works correctly", () => {
+    test("expect consonants button to hide vowels container", () => {
+        document.getElementById("consonants-button");
+        let vowelsContainer = document.getElementById("vowels-container");
+        conClick();
+        expect(vowelsContainer.classList.contains("nodisplay"));
+    });
+})
+
+describe("objects include correct values", () => {
+    test('object assignment', () => {
+        const vowels = {"kor": "ㅏ"};
+        vowels["eng"] = "a";
+        expect(vowels).toEqual({"kor": "ㅏ", "eng": "a"});
+        
+  });
+})
